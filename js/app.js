949430a3f8243a5cfda74af13d8e0938703b1825
ghanemgit <ghanem_97@outlook.com>
2022-01-21 13:03:06 +0200

    Request to re-enter the user if the entry is wrong

--- js/app.js
+++ js/app.js
@@ -11,24 +11,23 @@ while (true) {
         alert("Plese enter your Gender Correctly");
     }
 }
-
-var hisHerAge = prompt("Please enter your Age");
-
-if (hisHerAge <= 0) {
-    alert("Plese enter your Age correctly");
-
-}
-
-var hisHerAcceptToWelcoming = confirm("Are you want to show Welcoming Message");
-if (hisHerAcceptToWelcoming == true) {
-    switch (hisHerGender) {
-        case "male":
-            alert("Welcome Mr " + hisHerName);
-            break;
-        case "female":
-            alert("Welcome Ms " + hisHerName);
-            break;
-        default:
-            alert(hisHerName);
+    while (true) {
+        var hisHerAge = prompt("Please enter your Age");
+        if (hisHerAge > 0) break;
+        else {
+            alert("Plese enter your Age correctly");
+        }
     }
-}
+        var hisHerAcceptToWelcoming = confirm("Are you want to show Welcoming Message");
+        if (hisHerAcceptToWelcoming == true) {
+            switch (hisHerGender) {
+                case "male":
+                    alert("Welcome Mr " + hisHerName);
+                    break;
+                case "female":
+                    alert("Welcome Ms " + hisHerName);
+                    break;
+                default:
+                    alert(hisHerName);
+            }
+        }
